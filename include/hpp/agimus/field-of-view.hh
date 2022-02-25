dbd5bd4d85079fd776ff413f421ef2a4aa9f165e
Diane Bury <diane.bury@laas.fr>
2022-02-25 14:34:49 +0000

    [FieldOfView] Initial commit

---
+++ include/hpp/agimus/field-of-view.hh
@@ -0,0 +1,100 @@
+// Copyright 2022, CNRS, Airbus SAS
+
+// Author: Diane Bury
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+
+// 2. Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following
+// disclaimer in the documentation and/or other materials provided
+// with the distribution.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef HPP_AGIMUS_FIELD_OF_VIEW_HH
+#define HPP_AGIMUS_FIELD_OF_VIEW_HH
+
+#include <hpp/util/pointer.hh>
+#include <hpp/agimus/fwd.hh>
+#include <hpp/fcl/data_types.h>
+
+
+namespace hpp {
+  namespace agimus {
+    using hpp::fcl::Triangle;
+
+    typedef std::vector<Triangle> Tetahedron;
+
+    class Feature
+    {
+    public:
+      Feature(std::string& name, value_type size):
+        name(name), size(size) {}
+
+      std::string name;
+      value_type size;
+    }; // class Feature
+
+    class FeatureGroup
+    {
+    private:
+      std::vector<Feature> features;
+      int n_visibility_thr;
+      value_type depth_margin;
+      value_type size_margin;
+
+    public:
+      FeatureGroup(int n_visibility_thr, value_type depth_margin,
+                   value_type size_margin)
+          : n_visibility_thr(n_visibility_thr),
+          depth_margin(depth_margin),
+          size_margin(size_margin)
+        {};
+    }; // class FeatureGroup
+    typedef std::vector <FeatureGroup> FeatureGroups_t;
+
+    class FieldOfView
+    {
+    public:
+      static FieldOfViewPtr_t create(const ProblemSolverPtr_t& ps)
+      {
+        FieldOfViewPtr_t ptr (new FieldOfView(ps));
+        ptr->init(ptr);
+        return ptr;
+      }
+      
+    private:
+      // Constructor
+      FieldOfView(const ProblemSolverPtr_t& ps);
+      void init(const FieldOfViewWkPtr_t)
+      {}
+
+      void featureToTetahedrontPts();
+      bool featureVisible();
+      bool robotClogsFieldOfView();
+      bool clogged();
+
+      ProblemSolverPtr_t problemSolver_;
+      bool display_;
+
+    }; // class FieldOfView
+  } // namespace agimus
+} // namespace hpp
+
+#endif // HPP_AGIMUS_FIELD_OF_VIEW_HH
--- src/field-of-view.cc
+++ src/field-of-view.cc
@@ -1,6 +1,6 @@
 // Copyright 2022, CNRS, Airbus SAS
 
-// Author: Florent Lamiraux
+// Author: Diane Bury
 
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
@@ -27,31 +27,39 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 // OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef AGIMUS_HPP_FWD_HH
-#define AGIMUS_HPP_FWD_HH
+#include <hpp/agimus/field-of-view.hh>
 
-#include <hpp/manipulation/fwd.hh>
+namespace hpp {
+  namespace agimus {
 
-namespace hpp{
-namespace agimus{
+    FieldOfView::FieldOfView(const ProblemSolverPtr_t& ps):
+      problemSolver_ (ps)
+      {}
 
-  typedef pinocchio::DevicePtr_t DevicePtr_t;
-  typedef pinocchio::Frame Frame;
-  typedef pinocchio::FrameIndex FrameIndex;
-  typedef pinocchio::GeomIndex GeomIndex;
-  typedef pinocchio::JointIndex JointIndex;
-  typedef pinocchio::JointPtr_t JointPtr_t;
-  typedef pinocchio::size_type size_type;
-  typedef pinocchio::Transform3f Transform3f;
-  typedef pinocchio::vector3_t vector3_t;
-  typedef pinocchio::value_type value_type;
-  typedef pinocchio::vector_t vector_t;
-  typedef manipulation::ProblemSolverPtr_t ProblemSolverPtr_t;
-  HPP_PREDEF_CLASS(PointCloud);
-  typedef shared_ptr<PointCloud> PointCloudPtr_t;
-  HPP_PREDEF_CLASS(FieldOfView);
-  typedef shared_ptr<FieldOfView> FieldOfViewPtr_t;
-  typedef Eigen::Matrix<value_type, Eigen::Dynamic, 3> PointMatrix_t;
-} // namespace agimus
+    
+      void FieldOfView::featureToTetahedrontPts()
+      {
+
+      }
+
+      bool FieldOfView::featureVisible()
+      {
+        bool visible = false;
+        return visible;
+      }
+
+      bool FieldOfView::robotClogsFieldOfView()
+      {
+        bool clogged = false;
+        return clogged;
+      }
+
+      bool FieldOfView::clogged()
+      {
+        bool clogged = false;
+        return clogged;
+      }
+
+
+  } // namespace agimus
 } // namespace hpp
-#endif // AGIMUS_HPP_FWD_HH
